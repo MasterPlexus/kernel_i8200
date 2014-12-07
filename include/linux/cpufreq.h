cdbe0c287129e29270fb01d475667187d49b5b9c
Masterplexus <nospamming@web.de>
2014-12-07 18:26:20 +0100

    switch all additive govenors out

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -370,15 +370,16 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+/* put it off
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LULZACTIVE)
 extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lulzactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
-/* put it off
+*#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 *extern struct cpufreq_governor cpufreq_gov_smartass2;
 *#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass2)
-*#endif
 */
+#endif
+
 
 /*********************************************************************
  *                     FREQUENCY TABLE HELPERS                       *
